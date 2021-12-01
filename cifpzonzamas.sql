5096a361521d1776963ea85f294250afa42e36cd
elramones13 <57669978+elramones13@users.noreply.github.com>
2021-12-01 20:47:57 +0000

    version_3

--- cifpzonzamas.sql
+++ cifpzonzamas.sql
@@ -124,56 +124,6 @@ CREATE TABLE log_modulos (
     id_prof int
 );
 
-/* ==================================== RULES PARA LOGS DE ALUMNOS ==================================== */
-CREATE OR REPLACE RULE insert_alum_zonzamas AS 
-ON INSERT TO alumnos DO ALSO 
-INSERT INTO log_alumnos (accion,id_alumno,repetidor,id_curso_ciclo,nombre,apellido1,apellido2,fecha_nacimiento,email,telefono,sexo,pais) 
-VALUES ('INSERT',NEW.id_alumnos,NEW.repetidor,NEW.id_curso_ciclo,NEW.nombre,NEW.apellido1,NEW.apellido2,NEW.fecha_nacimiento,NEW.email,NEW.telefono,NEW.sexo,NEW.pais);
-
-CREATE OR REPLACE RULE update_alum_zonzamas AS 
-ON UPDATE TO alumnos DO ALSO 
-INSERT INTO log_alumnos (accion,id_alumno,repetidor,id_curso_ciclo,nombre,apellido1,apellido2,fecha_nacimiento,email,telefono,sexo,pais) 
-VALUES ('UPDATE',NEW.id_alumnos,NEW.repetidor,NEW.id_curso_ciclo,NEW.nombre,NEW.apellido1,NEW.apellido2,NEW.fecha_nacimiento,NEW.email,NEW.telefono,NEW.sexo,NEW.pais);
-
-CREATE OR REPLACE RULE delete_alum_zonzamas AS 
-ON DELETE TO alumnos DO ALSO 
-INSERT INTO log_alumnos (accion,id_alumno,repetidor,id_curso_ciclo,nombre,apellido1,apellido2,fecha_nacimiento,email,telefono,sexo,pais) 
-VALUES ('DELETE',OLD.id_alumnos,OLD.repetidor,OLD.id_curso_ciclo,OLD.nombre,OLD.apellido1,OLD.apellido2,OLD.fecha_nacimiento,OLD.email,OLD.telefono,OLD.sexo,OLD.pais);
-/* ======================================================================================================= */
-
-/* ==================================== RULES PARA LOGS DE PROFESORES ==================================== */
-CREATE OR REPLACE RULE insert_prof_zonzamas AS 
-ON INSERT TO profesores DO ALSO 
-INSERT INTO log_profesores (accion,id_alumno,nombre,apellido1,apellido2,fecha_nacimiento,email,telefono,sexo,pais) 
-VALUES ('INSERT',NEW.id_profesor,NEW.nombre,NEW.apellido1,NEW.apellido2,NEW.fecha_nacimiento,NEW.email,NEW.telefono,NEW.sexo,NEW.pais);
-
-CREATE OR REPLACE RULE update_prof_zonzamas AS 
-ON UPDATE TO profesores DO ALSO 
-INSERT INTO log_profesores (accion,id_alumno,nombre,apellido1,apellido2,fecha_nacimiento,email,telefono,sexo,pais) 
-VALUES ('UPDATE',NEW.id_profesor,NEW.nombre,NEW.apellido1,NEW.apellido2,NEW.fecha_nacimiento,NEW.email,NEW.telefono,NEW.sexo,NEW.pais);
-
-CREATE OR REPLACE RULE delete_prof_zonzamas AS 
-ON DELETE TO profesores DO ALSO 
-INSERT INTO log_profesores (accion,id_alumno,nombre,apellido1,apellido2,fecha_nacimiento,email,telefono,sexo,pais) 
-VALUES ('DELETE',OLD.id_profesor,OLD.nombre,OLD.apellido1,OLD.apellido2,OLD.fecha_nacimiento,OLD.email,OLD.telefono,OLD.sexo,OLD.pais);
-/* ======================================================================================================= */
-
-/* ==================================== RULES PARA LOGS DE MODULOS ==================================== */
-CREATE OR REPLACE RULE insert_mod_zonzamas AS 
-ON INSERT TO modulos DO ALSO 
-INSERT INTO log_modulos (accion,nombre,siglas,id_curso_ciclo,id_prof)
-VALUES ('INSERT',NEW.nombre,NEW.siglas,NEW.id_curso_ciclo,NEW.id_prof);
-
-CREATE OR REPLACE RULE update_mod_zonzamas AS 
-ON UPDATE TO modulos DO ALSO 
-INSERT INTO log_modulos (accion,nombre,siglas,id_curso_ciclo, id_prof) 
-VALUES ('UPDATE',NEW.nombre,NEW.siglas,NEW.id_curso_ciclo,NEW.id_prof);
-
-CREATE OR REPLACE RULE delete_mod_zonzamas AS 
-ON DELETE TO modulos DO ALSO 
-INSERT INTO log_modulos (accion,nombre,siglas, id_curso_ciclo, id_prof) 
-VALUES ('DELETE',OLD.nombre,OLD.siglas,OLD.id_curso_ciclo,OLD.id_prof);
-/* ======================================================================================================= */
 
 CREATE OR REPLACE RULE actu_cursos_ciclos1 AS ON UPDATE TO alumnos where NEW.id_curso_ciclo = 1  DO ALSO insert into alumnos_modulos (id_alumno, id_modulo) VALUES (NEW.id_copy, 1), (NEW.id_copy, 2), (NEW.id_copy, 3), (NEW.id_copy, 4), (NEW.id_copy, 5), (NEW.id_copy, 6);
 CREATE OR REPLACE RULE actu_cursos_ciclos2 AS ON UPDATE TO alumnos where NEW.id_curso_ciclo = 2  DO ALSO insert into alumnos_modulos (id_alumno, id_modulo) VALUES (NEW.id_copy, 7), (NEW.id_copy, 8), (NEW.id_copy, 9), (NEW.id_copy, 10), (NEW.id_copy, 11);
@@ -524,3 +474,53 @@ insert into alumnos (nombre, apellido1, apellido2, fecha_nacimiento, email, tele
 
 
 UPDATE alumnos SET id_copy = id;
+/* ==================================== RULES PARA LOGS DE ALUMNOS ==================================== */
+CREATE OR REPLACE RULE insert_alum_zonzamas AS 
+ON INSERT TO alumnos DO ALSO 
+INSERT INTO log_alumnos (accion,id_alumno,repetidor,id_curso_ciclo,nombre,apellido1,apellido2,fecha_nacimiento,email,telefono,sexo,pais) 
+VALUES ('INSERT',NEW.id_alumnos,NEW.repetidor,NEW.id_curso_ciclo,NEW.nombre,NEW.apellido1,NEW.apellido2,NEW.fecha_nacimiento,NEW.email,NEW.telefono,NEW.sexo,NEW.pais);
+
+CREATE OR REPLACE RULE update_alum_zonzamas AS 
+ON UPDATE TO alumnos DO ALSO 
+INSERT INTO log_alumnos (accion,id_alumno,repetidor,id_curso_ciclo,nombre,apellido1,apellido2,fecha_nacimiento,email,telefono,sexo,pais) 
+VALUES ('UPDATE',NEW.id_alumnos,NEW.repetidor,NEW.id_curso_ciclo,NEW.nombre,NEW.apellido1,NEW.apellido2,NEW.fecha_nacimiento,NEW.email,NEW.telefono,NEW.sexo,NEW.pais);
+
+CREATE OR REPLACE RULE delete_alum_zonzamas AS 
+ON DELETE TO alumnos DO ALSO 
+INSERT INTO log_alumnos (accion,id_alumno,repetidor,id_curso_ciclo,nombre,apellido1,apellido2,fecha_nacimiento,email,telefono,sexo,pais) 
+VALUES ('DELETE',OLD.id_alumnos,OLD.repetidor,OLD.id_curso_ciclo,OLD.nombre,OLD.apellido1,OLD.apellido2,OLD.fecha_nacimiento,OLD.email,OLD.telefono,OLD.sexo,OLD.pais);
+/* ======================================================================================================= */
+
+/* ==================================== RULES PARA LOGS DE PROFESORES ==================================== */
+CREATE OR REPLACE RULE insert_prof_zonzamas AS 
+ON INSERT TO profesores DO ALSO 
+INSERT INTO log_profesores (accion,id_alumno,nombre,apellido1,apellido2,fecha_nacimiento,email,telefono,sexo,pais) 
+VALUES ('INSERT',NEW.id_profesor,NEW.nombre,NEW.apellido1,NEW.apellido2,NEW.fecha_nacimiento,NEW.email,NEW.telefono,NEW.sexo,NEW.pais);
+
+CREATE OR REPLACE RULE update_prof_zonzamas AS 
+ON UPDATE TO profesores DO ALSO 
+INSERT INTO log_profesores (accion,id_alumno,nombre,apellido1,apellido2,fecha_nacimiento,email,telefono,sexo,pais) 
+VALUES ('UPDATE',NEW.id_profesor,NEW.nombre,NEW.apellido1,NEW.apellido2,NEW.fecha_nacimiento,NEW.email,NEW.telefono,NEW.sexo,NEW.pais);
+
+CREATE OR REPLACE RULE delete_prof_zonzamas AS 
+ON DELETE TO profesores DO ALSO 
+INSERT INTO log_profesores (accion,id_alumno,nombre,apellido1,apellido2,fecha_nacimiento,email,telefono,sexo,pais) 
+VALUES ('DELETE',OLD.id_profesor,OLD.nombre,OLD.apellido1,OLD.apellido2,OLD.fecha_nacimiento,OLD.email,OLD.telefono,OLD.sexo,OLD.pais);
+/* ======================================================================================================= */
+
+/* ==================================== RULES PARA LOGS DE MODULOS ==================================== */
+CREATE OR REPLACE RULE insert_mod_zonzamas AS 
+ON INSERT TO modulos DO ALSO 
+INSERT INTO log_modulos (accion,nombre,siglas,id_curso_ciclo,id_prof)
+VALUES ('INSERT',NEW.nombre,NEW.siglas,NEW.id_curso_ciclo,NEW.id_prof);
+
+CREATE OR REPLACE RULE update_mod_zonzamas AS 
+ON UPDATE TO modulos DO ALSO 
+INSERT INTO log_modulos (accion,nombre,siglas,id_curso_ciclo, id_prof) 
+VALUES ('UPDATE',NEW.nombre,NEW.siglas,NEW.id_curso_ciclo,NEW.id_prof);
+
+CREATE OR REPLACE RULE delete_mod_zonzamas AS 
+ON DELETE TO modulos DO ALSO 
+INSERT INTO log_modulos (accion,nombre,siglas, id_curso_ciclo, id_prof) 
+VALUES ('DELETE',OLD.nombre,OLD.siglas,OLD.id_curso_ciclo,OLD.id_prof);
+/* ======================================================================================================= */
