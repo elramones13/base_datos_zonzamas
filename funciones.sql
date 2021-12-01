c025d961e987c5e9c2e40dbd7ebf9d5d84d6df2a
elramones13 <57669978+elramones13@users.noreply.github.com>
2021-12-01 20:59:05 +0000

    version_2

--- funciones.sql
+++ funciones.sql
@@ -1,12 +1,3 @@
-do
-    'declare
-        n_alumnos integer;
-    begin
-        select count(id_alumno)  into n_alumnos
-        from alumnos_modulos where id_modulo=1;
-        raise notice ''El numero de alumnos es: %'', n_alumnos;
-end;';
-
 create function contador_alumnos(
     id integer
 ) returns bigint
@@ -25,6 +16,6 @@ do
         if n_alumnos <= 0 then
             raise notice ''El numero de alumnos es 0'';
             else
-            raise notice ''El numero de alumnos es: %'', n_alumnos;
+            raise notice ''El numero de alumnos en el modulo con Id=1 es: %'', n_alumnos;
         END if;
 end;';
