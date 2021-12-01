74f65f3362f66abb73d6fac91f3a8de3b60f6ca9
elramones13 <57669978+elramones13@users.noreply.github.com>
2021-12-01 21:59:45 +0000

    locura

--- funciones.sql
+++ funciones.sql
@@ -5,7 +5,7 @@ do
         n_alumnos integer;
     begin
         select count(id_alumno)  into n_alumnos
-        from alumnos_modulos where id_modulo=1;
+        from alumnos_modulos where id_modulo=3;
         raise notice ''El numero de alumnos en el modulo es: %'', n_alumnos;
 end;';
 
@@ -14,7 +14,7 @@ create function contador_alumnos(
 ) returns bigint
 language SQL as 
 $$
-    select count(id_alumno) from alumnos_modulos where id_modulo=id;
+    select count(id_alumno) from alumnos_modulos where id_modulo=3;
 $$;
 
 
@@ -28,5 +28,24 @@ do
             raise notice ''El numero de alumnos es 0'';
             else
             raise notice ''El numero de alumnos es: %'', n_alumnos;
+        END if;
+end;';
+
+select count(id_alumno) from alumnos_modulos am, alumnos a where am.id_alumno=a.id_alumnos and am.id_modulo=1 and a.sexo='M';
+select count(am.id_alumno), a.sexo, a.pais, a.id_curso_ciclo from alumnos_modulos am, alumnos a where am.id_alumno=a.id_alumnos and am.id_modulo=1 and a.sexo='F' group by am.id_alumno, a.sexo, a.pais, a.id_curso_ciclo;
+select am.id_alumno, a.sexo, a.pais, a.id_curso_ciclo from alumnos_modulos am, alumnos a where am.id_alumno=a.id_alumnos and am.id_modulo=1 and a.sexo='F' group by am.id_alumno, a.sexo, a.pais, a.id_curso_ciclo;
+
+
+
+do
+    'declare
+        grado integer;
+    begin
+        select am.id_alumno, a.sexo, a.pais, a.id_curso_ciclo into grado 
+        from alumnos_modulos am, alumnos a where am.id_alumno=a.id_alumnos and am.id_modulo=8 group by am.id_alumno, a.sexo, a.pais, a.id_curso_ciclo;
+        if grado <=4 then
+            raise notice ''Grado superior'';
+            else
+            raise notice ''Grado medio'';
         END if;
 end;';
