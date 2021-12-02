c07e595ce0b44cc41604c685044e5723a8379566
elramones13 <57669978+elramones13@users.noreply.github.com>
2021-12-02 19:14:41 +0000

    version_4

--- funciones.sql
+++ funciones.sql
@@ -2,17 +2,76 @@ drop function contador_alumnos;
 
 do
     'declare
-        n_alumnos integer;
+        n_alumnos integer :=0;
+	modulo integer := 30;
+	sexo VARCHAR:=''F'';
+	tipo VARCHAR:=''GS'';
+	pais VARCHAR:=''RU'';
     begin
-        select count(id_alumno)  into n_alumnos
-        from alumnos_modulos where id_modulo=3;
-        raise notice ''El numero de alumnos en el modulo es: %'', n_alumnos;
+        select count(*)  into n_alumnos
+        from alumnos a, alumnos_modulos am where a.id_alumnos=am.id_alumno and am.id_modulo=modulo and a.sexo=''F'';
+        raise notice ''El modulo es: %'', modulo;
+	raise notice ''El numero de alumnos es: %'', n_alumnos;
 end;';
+
+
+/* ================================ FUNCIONES ================================ */
+
+do
+    'declare
+        n_alumnos integer :=0;
+	modulo integer := 30;
+	sexo_sel VARCHAR:=''M'';
+	tipo_sel VARCHAR:=''GS'';
+    begin
+        select count(*) into n_alumnos 
+	from alumnos_modulos am
+             ,alumnos a
+             ,cursos_ciclos cc
+             ,ciclos c 
+ 	where am.id_alumno=a.id_alumnos 
+        and am.id_modulo=modulo 
+        and a.sexo= sexo_sel
+ 	and a.id_curso_ciclo=cc.id_ciclo and cc.id_ciclo=c.id and c.tipo= tipo_sel;
+	raise notice ''El numero de alumnos es: %'', n_alumnos;
+
+end;';
+
+
+do $$
+    declare
+        n_alumnos integer :=0;
+	modulo integer := 30;
+	sexo_sel VARCHAR:='M';
+	tipo_sel VARCHAR:='GS';
+    begin
+        select count(*) into n_alumnos 
+	from alumnos_modulos am
+             ,alumnos a
+             ,cursos_ciclos cc
+             ,ciclos c 
+ 	where am.id_alumno=a.id_alumnos 
+        and am.id_modulo=modulo 
+        and a.sexo= sexo_sel
+ 	and a.id_curso_ciclo=cc.id_ciclo and cc.id_ciclo=c.id and c.tipo= tipo_sel;
+	raise notice 'El numero de alumnos es: %', n_alumnos;
+
+end;
+$$;
+
+/* ========================================================================== */
+
+
+select count(*) from alumnos_modulos am, alumnos a, cursos_ciclos cc, ciclos c 
+ where am.id_alumno=a.id_alumnos and am.id_modulo=30 and a.sexo='F' 
+ and a.id_curso_ciclo=cc.id_ciclo and cc.id_ciclo=c.id and c.tipo='GS';
+
+
 
 create function contador_alumnos(
     id integer
 ) returns bigint
-language SQL as 
+language plpgsql as 
 $$
     select count(id_alumno) from alumnos_modulos where id_modulo=3;
 $$;
@@ -35,7 +94,7 @@ select count(id_alumno) from alumnos_modulos am, alumnos a where am.id_alumno=a.
 select count(am.id_alumno), a.sexo, a.pais, a.id_curso_ciclo from alumnos_modulos am, alumnos a where am.id_alumno=a.id_alumnos and am.id_modulo=1 and a.sexo='F' group by am.id_alumno, a.sexo, a.pais, a.id_curso_ciclo;
 select am.id_alumno, a.sexo, a.pais, a.id_curso_ciclo from alumnos_modulos am, alumnos a where am.id_alumno=a.id_alumnos and am.id_modulo=1 and a.sexo='F' group by am.id_alumno, a.sexo, a.pais, a.id_curso_ciclo;
 
-
+select count(*) from alumnos_modulos am, alumnos a, cursos_ciclos cc, ciclos c where am.id_alumno=a.id_alumnos and am.id_modulo=30 and a.sexo='F' and a.id_curso_ciclo=cc.id_ciclo and cc.id_ciclo=c.id;
 
 do
     'declare
